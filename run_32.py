64ff28e9404d038b7dfabde4efce4668bd1d45ad
Arif <arif@themascoteers.com>
2025-03-28 21:39:29 +1100

    hello

--- run_32.py
+++ run_32.py
@@ -2,10 +2,10 @@ from task_runner import TaskRunner
 
 if __name__ == '__main__':
     train_sizes = [0.75]
-    verbose = False
-    tag = f"10_best_75"
+    verbose = True
+    tag = f"run_32_1"
     tasks = {
-        "algorithms": ["bsdrcnn_r"],
+        "algorithms": ["transformer1"],
         "datasets": ["lucas"],
         "target_sizes": [32],
         "scale_y": ["robust"],
