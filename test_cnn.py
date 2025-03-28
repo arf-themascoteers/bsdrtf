95eb89b25943a2111a30a27e1300caa85baced4e
Arif <arif@themascoteers.com>
2025-03-29 09:51:45 +1100

    hello

--- test_cnn.py
+++ test_cnn.py
@@ -3,9 +3,8 @@ import torch.nn as nn
 
 
 cnn = nn.Sequential(
-    nn.Conv1d(1, 8, kernel_size=8, stride=1, padding=0),
+    nn.Conv1d(1, 24, kernel_size=32, stride=32, padding=0),
     nn.LeakyReLU(),
-    nn.MaxPool1d(kernel_size=8, stride=8, padding=0),
     nn.Flatten(start_dim=1),
     nn.LayerNorm(24)
 )
